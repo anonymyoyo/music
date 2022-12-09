405035f6b1b127491bc04dbb075548a1cb0b100a
anonymyoyo <paulemmanuelbohole5@gmail.com>
2022-12-09 13:21:45 +0100

    update

---
+++ body/header.php
@@ -0,0 +1,445 @@
+<link rel="stylesheet" href="../css/style.css">
+<!-- <i class="fa-thin fa-music"></i> -->
+
+<header>
+    <div class="menu_side">
+        <h1>Playlist</h1>
+        <div class="playlist"> 
+            <h4 class="active"><span></span> <i class="bi bi-music-note-beamed"></i>Playlist</h4>
+            <h4><span></span><i class="bi bi-music-note-beamed"></i> Last Listening</h4>
+            <h4><span></span><i class="bi bi-music-note-beamed"></i> Recommended</h4>
+        </div>
+        <div class="menu_song">
+            <li class="songItem">
+                <span>01</span>
+                <img src="../image/damso.png" alt="">
+                <h5>Humain 
+                    <div class="subtitle">Damso</div>
+                </h5>
+                <i class="bi playlistPlay bi-play-circle-fill" id="1"></i>
+            </li>
+
+            <li class="songItem">
+                <span>02</span>
+                <img src="../image/ninho.png" alt="">
+                <h5>Filon
+                    <div class="subtitle">Ninho</div>
+                </h5>
+                <i class="bi playlistPlay bi-play-circle-fill" id="2"></i>
+            </li>
+
+            <li class="songItem">
+                <span>03</span>
+                <img src="../image/dadju.png" alt="">
+                <h5>Robes 
+                    <div class="subtitle">Dadju</div>
+                </h5>
+                <i class="bi playlistPlay bi-play-circle-fill" id="3"></i>
+            </li>
+
+            <li class="songItem">
+                <span>04</span>
+                <img src="../image/destin.png" alt="">
+                <h5>Goutte d'eau
+                    <div class="subtitle">Ninho</div>
+                </h5>
+                <i class="bi playlistPlay bi-play-circle-fill" id="4"></i>
+            </li>
+
+            <li class="songItem">
+                <span>05</span>
+                <img src="../image/gotham.png" alt="">
+                <h5>Gotham 
+                    <div class="subtitle">Booba</div>
+                </h5>
+                <i class="bi playlistPlay bi-play-circle-fill" id="5"></i>
+            </li>
+
+            <li class="songItem">
+                <span>06</span>
+                <img src="../image/mood.png" alt="">
+                <h5>Petit Diamant 
+                    <div class="subtitle">Franglish</div>
+                </h5>
+                <i class="bi playlistPlay bi-play-circle-fill" id="6"></i>
+            </li>
+
+            <li class="songItem">
+                <span>07</span>
+                <img src="../image/ronisia.png" alt="">
+                <h5>Suis-moi 
+                    <div class="subtitle">Ronisia</div>
+                </h5>
+                <i class="bi playlistPlay bi-play-circle-fill" id="7"></i>
+            </li>
+
+            <li class="songItem">
+                <span>08</span>
+                <img src="../image/ultra.png" alt="">
+                <h5>Grain de sable 
+                    <div class="subtitle">Booba</div>
+                </h5>
+                <i class="bi playlistPlay bi-play-circle-fill" id="8"></i>
+            </li>
+
+            <li class="songItem">
+                <span>09</span>
+                <img src="../image/ultra.png" alt="">
+                <h5>Mona Lisa
+                    <div class="subtitle">Booba</div>
+                </h5>
+                <i class="bi playlistPlay bi-play-circle-fill" id="9"></i>
+            </li>
+
+            <li class="songItem">
+                <span>10</span>
+                <img src="../image/ronisia.png" alt="">
+                <h5>Suis-moi 
+                    <div class="subtitle">Ronisia</div>
+                </h5>
+                <i class="bi playlistPlay bi-play-circle-fill" id="10"></i>
+            </li>
+
+            <li class="songItem">
+                <span>11</span>
+                <img src="../image/ronisia.png" alt="">
+                <h5>Suis-moi 
+                    <div class="subtitle">Ronisia</div>
+                </h5>
+                <i class="bi playlistPlay bi-play-circle-fill" id="11"></i>
+            </li>
+
+            <li class="songItem">
+                <span>12</span>
+                <img src="../image/ronisia.png" alt="">
+                <h5>Suis-moi 
+                    <div class="subtitle">Ronisia</div>
+                </h5>
+                <i class="bi playlistPlay bi-play-circle-fill" id="12"></i>
+            </li>
+
+            <li class="songItem">
+                <span>13</span>
+                <img src="../image/ronisia.png" alt="">
+                <h5>Suis-moi 
+                    <div class="subtitle">Ronisia</div>
+                </h5>
+                <i class="bi playlistPlay bi-play-circle-fill" id="13"></i>
+            </li>
+        </div>
+    </div>
+
+    <div class="song_side">
+        <nav>
+            <ul>
+                <li>Discover <span></span></li>
+                <li>My Library</li>
+                <li>Radio</li>
+            </ul>
+            <div class="search">
+                <i class="bi bi-search"></i>
+                <input type="text" name="" placeholder="Search Music..." id="">
+            </div>
+            <div class="user">
+                <img src="../image/destin.png" alt="">
+            </div>
+        </nav>
+        <div class="content">
+            <h1>Damso- <span>Humain</span></h1>
+            <h3>Paroles de chansons -</h3>
+            <p>
+                Vil à temps, violentant, fin méchant, président, tout un temps<br>
+                Billets, pan, voletant, vole au vent, survolant livraison<br>
+                Gilet blanc tacheté d'sang, caché dans casier d'adolescent<br>
+                Cardigan, Louis Vuitton pour deux francs, montant d'fabrication<br>
+                Prisonnier isolé réveillé par biais de chiffonnier<br>
+                Salarié sous-payé, avarié, soudoyé, suicidé<br>
+                Lampadaire sans lumière éclaire SDF et bandoulière<br>
+                Antipode de la mort d'handicos peu glamour, visionnaire<br>
+                Tatouage insensés, cafouillages de pensées tout l'été<br>
+                Dénudé pour du blé, trémoussé sous l'effet de la C<br>
+                Policier étranger dérangé pour c'qu'il est, peau foncée<br>
+                Odyssée mystifiée, Colisée réparé, offensé (Dems)<br>
+                Qui sont-ils quand ils n'ont jamais vraiment été ?<br><br>
+
+                Personne n'a déjà rêvé de naître<br>
+                Perdu dans le berceau, je n'ai pas cessé de naître<br>
+                J'ai couru dans mon cerveau jusqu'au fond de mon être<br>
+                J'ai trouvé loin d'mes vaisseaux tout c'qui faisait mon être<br>
+                (yeah yeah yeah yeah yeah)<br>
+                Personne n'a déjà rêvé de naître<br>
+                Perdu dans le berceau, je n'ai pas cessé de naître<br>
+                J'ai couru dans mon cerveau jusqu'au fond de mon être<br>
+                J'ai trouvé loin d'mes vaisseaux tout c'qui faisait mon être<br>
+                (yeah yeah yeah yeah yeah)<br>
+                <br>
+                De la gériatrie à la maternité, des félicités aux RIP<br>
+                On fait tout d'avance parce qu'il n'y a pas d'replay<br>
+                Tout peut s'arrêter, les deux barres parallèles sont des carrés dans la réalité<br>
+                Toujours pas niqué car toujours paniqué, vie, mort, mensonge et vérité<br>
+                Une infinité de fins ou une fin à une infinité<br>
+                Déguisé en être humain pour un semblant d'humanité<br>
+                Charnelles sont les générosités pour hypnotiser les gens pendant qu'mendiants immigrés<br>
+                S'mettent à copiner des clans, s'mettent à cotiser des camps où sont entassés des gens<br>
+                J'crois qu'le plus intelligent s'rait d'avouer qu'on est con<br>
+                Serait d'avouer qu'on est blanc, noir ou quel que soit le ton<br>
+                Voir ce que nous laisse le temps, croire en nous mais sans le "en"<br>
+                Car qui sommes nous vraiment quand on l'est pas vraiment ?<br>
+                <br>
+                Personne n'a déjà rêvé de naître<br>
+                Perdu dans le berceau, je n'ai pas cessé de naître<br>
+                J'ai couru dans mon cerveau jusqu'au fond de mon être<br>
+                J'ai trouvé loin d'mes vaisseaux tout c'qui faisait mon être<br>
+                (yeah yeah yeah yeah yeah)<br>
+                Personne n'a déjà rêvé de naître<br>
+                Perdu dans le berceau, je n'ai pas cessé de naître<br>
+                J'ai couru dans mon cerveau jusqu'au fond de mon être<br>
+                J'ai trouvé loin d'mes vaisseaux tout c'qui faisait mon être<br>
+                (yeah yeah yeah yeah yeah)<br>
+                <br>
+                Humain, j'suis plus trop humain mais plus que humains quand j'vois l'humain<br>
+                Puis soudain, plus rien, y a plus trop d'humains, que des terriens<br>
+                Sur un grand terrain, qui s'aiment, qui s'tuent, qui s'saignent pour rien, comme un chérubin<br>
+                La seule voie que j'suivrai, c'est celle de mes cordes vocales<br>
+                Quand on meurt, on n'fait pas appel, la vie a ses failles<br>
+                Factures, courriers et rappels poussent à faire du sale<br>
+                Ils ne se veulent pas du bien, l'humain s'fait du mal<br>
+                Pour être ensemble, devons-nous vivre séparément ?<br>
+                J'crois que dans l'ensemble, c'est plus facile quand on se ment<br>
+                Y a des gens comme moi qui n'savent plus trop ce qu'ils sont<br>
+                Qui s'posent la question : "Mais qui suis-je vraiment quand j'ne suis plus vraiment ?"<br>
+                <br>
+                Personne n'a déjà rêvé de naître<br>
+                Perdu dans le berceau, je n'ai pas cessé de naître<br>
+                J'ai couru dans mon cerveau jusqu'au fond de mon être<br>
+                J'ai trouvé loin d'mes vaisseaux tout c'qui faisait mon être<br>
+                (yeah yeah yeah yeah yeah)<br>
+                Personne n'a déjà rêvé de naître<br>
+                Perdu dans le berceau, je n'ai pas cessé de naître<br>
+                J'ai couru dans mon cerveau jusqu'au fond de mon être<br>
+                J'ai trouvé loin d'mes vaisseaux tout c'qui faisait mon être<br>
+                (yeah yeah yeah yeah yeah)<br>
+                <br>
+                Yeah yeah<br>
+                Yeah yeah yeah yeah yeah yeah yeah<br>
+                Yeah yeah yeah yeah yeah yeah yeah<br>
+                Dems<br></p>
+
+            <div class="buttons">
+                <button><i class="bi bi-play-fill"></i> PLAY</button>
+                <button><i class="bi bi-person-plus-fill"></i> FOLLOW</button>
+                <button><i class="bi bi-share-fill"></i> SHARE</button>
+                <button><i class="bi bi-file-earmark-text-fill"></i> LYRICS</button>
+            </div>
+        <div class="song_cover">
+            <img src="../image/damso.png" alt="">
+        </div>
+        </div>
+
+        <div class="popular_song">
+            <div class="h4">
+                <h4>Popular Song</h4>
+                <div class="btn_s">
+                    <i class="bi bi-arrow-left-short" id="pop_song_left"></i>
+                    <i class="bi bi-arrow-right-short" id="pop_song_right"></i>
+                </div>
+            </div>
+            <div class="pop_song">
+                <li class="songItem">
+                    <div class="img_play">
+                        <img src="../image/popular song/GIMS.jpg" alt="">
+                        <i class="bi playlistPlay bi-play-circle-fill" id="14"></i>
+                    </div>
+                    <h5>THERMISTOCLE
+                        <div class="subtitle ">GIMS</div>
+                    </h5>
+                </li>
+
+                <li class="songItem">
+                    <div class="img_play">
+                        <img src="../image/popular song/Fresh.jpg" alt="">
+                        <i class="bi playlistPlay bi-play-circle-fill" id="15"></i>
+                    </div>
+                    <h5>KOBE
+                        <div class="subtitle ">FRESH</div>
+                    </h5>
+                </li>
+
+                <li class="songItem">
+                    <div class="img_play">
+                        <img src="../image/popular song/Popsmike1.jpg" alt="">
+                        <i class="bi playlistPlay bi-play-circle-fill" id="16"></i>
+                    </div>
+                    <h5>BOUT A MILLION
+                        <div class="subtitle ">POP SMOKE</divg>
+                    </h5>
+                </li>
+
+                <li class="songItem">
+                    <div class="img_play">
+                        <img src="../image/popular song/Niska.jpg" alt="">
+                        <i class="bi playlistPlay bi-play-circle-fill" id="17"></i>
+                    </div>
+                    <h5>GENKIDAMA
+                        <div class="subtitle ">NISKA</div>
+                    </h5>
+                </li>
+
+                <li class="songItem">
+                    <div class="img_play">
+                        <img src="../image/popular song/mood.png" alt="">
+                        <i class="bi playlistPlay bi-play-circle-fill" id="18"></i>
+                    </div>
+                    <h5>YOYO
+                        <div class="subtitle ">FRANGLISH</div>
+                    </h5>
+                </li>
+
+                <li class="songItem">
+                    <div class="img_play">
+                        <img src="../image/popular song/popsmoke3.jpg" alt="">
+                        <i class="bi playlistPlay bi-play-circle-fill" id="19"></i>
+                    </div>
+                    <h5>WHATS CRAKIN
+                        <div class="subtitle ">POP SMOKE</div>
+                    </h5>
+                </li>
+
+                <li class="songItem">
+                    <div class="img_play">
+                        <img src="../image/popular song/destin.png" alt="">
+                        <i class="bi playlistPlay bi-play-circle-fill" id="20"></i>
+                    </div>
+                    <h5>DESTIN
+                        <div class="subtitle ">NINHO</div>
+                    </h5>
+                </li>
+
+                <li class="songItem">
+                    <div class="img_play">
+                        <img src="../image/popular song/ninho.png" alt="">
+                        <i class="bi playlistPlay bi-play-circle-fill" id="21"></i>
+                    </div>
+                    <h5>FILON
+                        <div class="subtitle ">NINHO</div>
+                    </h5>
+                </li>
+
+                <li class="songItem">
+                    <div class="img_play">
+                        <img src="../image/ultra.png" alt="">
+                        <i class="bi playlistPlay bi-play-circle-fill" id="22"></i>
+                    </div>
+                    <h5>GRAIN DE SABLE
+                        <div class="subtitle ">BOOBA</div>
+                    </h5>
+                </li>
+
+                <li class="songItem">
+                    <div class="img_play">
+                        <img src="../image/ronisia.png" alt="">
+                        <i class="bi playlistPlay bi-play-circle-fill" id="23"></i>
+                    </div>
+                    <h5>ROBE
+                        <div class="subtitle ">DADJU</div>
+                    </h5>
+                </li>
+
+                <li class="songItem">
+                    <div class="img_play">
+                        <img src="../image/popular song/dadju.png" alt="">
+                        <i class="bi playlistPlay bi-play-circle-fill" id="23"></i>
+                    </div>
+                    <h5>ROBE
+                        <div class="subtitle ">DADJU</div>
+                    </h5>
+                </li>
+            </div>
+        </div>
+        <div class="popular_artists">
+            <div class="h4">
+                <h4>Popular Artist</h4>
+                <div class="btn_s">
+                    <i class="bi bi-arrow-left-short" id="pop_art_left"></i>
+                    <i class="bi bi-arrow-right-short" id="pop_art_right"></i>
+                </div>
+            </div>
+            <div class="item Artists_bx">
+                <li>
+                    <img src="../image/popular song/dadju.png" alt="">
+                </li>
+                <li>
+                    <img src="../image/popular song/destin.png" alt="">
+                </li>
+                <li>
+                    <img src="../image/popular song/Fresh.jpg" alt="">
+                </li>
+                <li>
+                    <img src="../image/popular song/GIMS.jpg" alt="">
+                </li>
+                <li>
+                    <img src="../image/popular song/mood.png" alt="">
+                </li>
+                <li>
+                    <img src="../image/popular song/Niska.jpg" alt="">
+                </li>
+                <li>
+                    <img src="../image/popular song/Popsmike1.jpg" alt="">
+                </li>
+                <li>
+                    <img src="../image/damso.png" alt="">
+                </li>
+                <li>
+                    <img src="../image/ultra.png" alt="">
+                </li>
+                <li>
+                    <img src="../image/ronisia.png" alt="">
+                </li>
+                <li>
+                    <img src="../image/popular song/Popsmike1.jpg" alt="">
+                </li>
+            </div>
+        </div>
+    </div>
+   
+    <div class="master_play">        
+        <div class="wave" id="wave">
+            <div class="wave1"></div>
+            <div class="wave1"></div>
+            <div class="wave1"></div>
+        </div>
+
+        <!-- cover, nom et titre du son encours de lecture  -->
+        <img src="../image/damso.png" alt="" id="poster_master_play">
+        <h5 id="title">
+            Damso 
+            <div class="subtitle">Humain</div>
+        </h5>
+
+        <!-- icones de controle et de telechargement des sons -->
+
+        <div class="icon">
+            <i class="bi shuffle bi-music-note-beamed">suivant</i>
+            <i class="bi bi-skip-start-fill"></i>
+            <i class="bi bi-play-fill" id="masterPlay"></i>
+            <i class="bi bi-skip-end-fill"></i>
+            <i class="bi bi-cloud-arrow-down-fill" id="download_music"></i>
+        </div>
+
+        <!-- barre de  controlle de temps -->
+
+        <span id="currentStart">0:00</span>
+        <div class="bar">
+            <input type="range" id="seek" min="0" max="100">
+            <div class="bar2" id="bar2"></div>
+            <div class="dot"></div>
+        </div>
+        <span id="currentEnd">0:00</span>
+        <div class="vol">
+            <i class="bi bi-volume-down-fill" id="vol_icon"></i>
+            <input type="range" min="0" max="100" id="vol">
+            <div class="vol_bar"></div>
+            <div class="dot" id="vol_bar"></div>
+        </div>
+    </div>
+</header>
